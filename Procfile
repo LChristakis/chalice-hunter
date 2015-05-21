2480a8f6792b88c5403b2bb31d02026f865899ed
Luke Christakis <LChristakis@gmail.com>
2015-05-21 18:06:43 -0400

    modified program to work with gunicorn

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn chalice-hunter:app --log-file=-
--- chalice-hunter.py
+++ chalice-hunter.py
@@ -18,11 +18,7 @@ css = Bundle(
 
 assets.register('css_all',css)
 
-
-chalice_list = []
-
 def fetch_chalice_list():
-    global chalice_list
     chalice_list = []
     #response  = requests.get('https://docs.google.com/spreadsheets/d/18TVy5cDk6sNX0lc2dNQM2Q-SLv0_9QecQrtKR0sRb6Q/export?format=tsv&id=18TVy5cDk6sNX0lc2dNQM2Q-SLv0_9QecQrtKR0sRb6Q&gid=1095946635') 
     #http://stackoverflow.com/questions/3305926/python-csv-string-to-array
@@ -36,15 +32,8 @@ def fetch_chalice_list():
                 if row[i]=='':
                     row[i]="None / Unsure"
             chalice_list.append(row)
+    return chalice_list
 
 @app.route("/")
 def index():
-    return render_template('index.html',data=chalice_list)
-
-@app.route("/query")
-def query():
-    return "JSON query"
-
-if __name__ == "__main__":
-    fetch_chalice_list()
-    app.run('0.0.0.0')
+    return render_template('index.html',data=fetch_chalice_list())
