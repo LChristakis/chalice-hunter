d07719b8725f35b2f09c4f48355fe641fc072da7
Luke Christakis <LChristakis@gmail.com>
2015-05-21 17:54:02 -0400

    Renamed app chalice-hunter. Updated, relocated tsv

--- bin/activate.fish
+++ bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/luke/chalice-seeker"
+set -gx VIRTUAL_ENV "/home/luke/chalice-hunter"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
---
+++ chalice-hunter.py
@@ -0,0 +1,50 @@
+from flask import Flask, jsonify, render_template, request
+from flask.ext.assets import Environment, Bundle
+from threading import Thread
+from io import StringIO
+#import requests
+import csv
+
+app = Flask(__name__)
+assets=Environment(app)
+
+
+css = Bundle(
+    Bundle('assets/surface_styles.scss',
+        filters='scss'),
+    Bundle('custom.css'),
+    filters='cssmin',
+    output='all.css')
+
+assets.register('css_all',css)
+
+
+chalice_list = []
+
+def fetch_chalice_list():
+    global chalice_list
+    chalice_list = []
+    #response  = requests.get('https://docs.google.com/spreadsheets/d/18TVy5cDk6sNX0lc2dNQM2Q-SLv0_9QecQrtKR0sRb6Q/export?format=tsv&id=18TVy5cDk6sNX0lc2dNQM2Q-SLv0_9QecQrtKR0sRb6Q&gid=1095946635') 
+    #http://stackoverflow.com/questions/3305926/python-csv-string-to-array
+    #reader = csv.reader(StringIO(response.text),delimiter='\t')
+    tsv_file = open('static/chalice_spreadsheet.tsv','r')
+    reader = csv.reader(tsv_file,delimiter='\t')
+    next(reader)
+    for row in reader:
+        if row[0] != '':
+            for i in range(4,12):
+                if row[i]=='':
+                    row[i]="None / Unsure"
+            chalice_list.append(row)
+
+@app.route("/")
+def index():
+    return render_template('index.html',data=chalice_list)
+
+@app.route("/query")
+def query():
+    return "JSON query"
+
+if __name__ == "__main__":
+    fetch_chalice_list()
+    app.run('0.0.0.0')
